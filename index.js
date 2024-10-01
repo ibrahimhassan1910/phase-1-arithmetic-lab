e135994e613d722d879107322e056d191fa54ac6
hassan <hassanibrahimndahiro@gmail.com>
2024-10-01 23:20:22 +0300

    initial

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
+const { expect } = require("chai");
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2;
+console.log(multiply);
+
+//test case
+expect(multiply).to.equal(62);
+
+// Random number generator
+function random() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+
+console.log(random());
+
+// Test case
+const randomNumber = random();
+expect(randomNumber).to.be.greaterThan(0);
+
+//export the random function
+module.exports = { random };
+
+
+  
